ccb8821f3e441e79ee332e165641b9735fd33ad1
llmel <contatoleonardomelo@hotmail.com>
2019-12-29 00:37:56 -0300

    Atualizado em 28/12/2019

---
+++ curso_em_video/mysql/script.sql
@@ -0,0 +1,17 @@
+create database cv_cadastro
+default character set utf8
+default collate utf8_general_ci;
+
+use cv_cadastro;
+
+create table cv_pessoas (
+	id int not null auto_increment,
+    nome varchar(30) not null,
+    nascimento date,
+    sexo enum('M','F'),
+    peso decimal(5,2),
+    altura decimal(3,2),
+    nacionalidade varchar(20) default 'Brasil',
+    primary key (id)
+) default charset = utf8;
+
